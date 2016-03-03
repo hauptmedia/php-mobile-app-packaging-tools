367760ca529928e7c0a81835fedd620c750cf4b1
Julian Haupt <julian.haupt@hauptmedia.de>
2016-03-03 19:25:48 +0100

    added AppleRequestFactory

---
+++ src/MobileAppPackagingTools/AppleRequestFactory.php
@@ -0,0 +1,39 @@
+<?php
+namespace MobileAppPackagingTools;
+
+use CFPropertyList\CFPropertyList;
+use CFPropertyList\CFTypeDetector;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AppleRequestFactory {
+    /**
+     * @param Request $request
+     * @return CFPropertyList|null
+     * @throws \CFPropertyList\IOException
+     * @throws \CFPropertyList\PListException
+     * @throws \DOMException
+     */
+    public static function parseProfileRequest(Request $request) {
+        $data = $request->getContent();
+
+        $plistBegin   = '<?xml version="1.0"';
+        $plistEnd   = '</plist>';
+
+        $pos1 = strpos($data, $plistBegin);
+        $pos2 = strpos($data, $plistEnd);
+
+        $plistData = substr ($data, $pos1, $pos2 - $pos1 + strlen($plistEnd));
+
+        $plist = null;
+
+        if($pos1 !== false && $pos2 !== false) {
+            $plist = new CFPropertyList();
+            $plist->parse($plistData, CFPropertyList::FORMAT_XML);
+        }
+
+        return $plist;
+    }
+
+
+}
