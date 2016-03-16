fe3d663f9d852cef54884e78cca2fb6533d49ee8
Julian Haupt <julian.haupt@hauptmedia.de>
2016-03-16 21:48:04 +0100

    added headers in ipaDownloadResponse

--- src/MobileAppPackagingTools/AppleResponseFactory.php
+++ src/MobileAppPackagingTools/AppleResponseFactory.php
@@ -25,9 +25,12 @@ class AppleResponseFactory {
     public static function createIpaDownloadResponse($filename) {
         $response = new Response();
 
+        $response->headers->set('Content-Type', 'application/octet-stream');
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Content-Disposition', 'attachment; filename="'.basename($filename).'"');
         $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
         $response->headers->set('Pragma', 'no-cache');
-        $response->headers->set('Content-Type', 'application/octet-stream');
+        $response->headers->set('Content-Length', filesize($filename));
 
         $response->setContent(file_get_contents($filename));
 
