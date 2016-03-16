a2355e6acc1f58f1e23f735200daca3f355e2aca
Julian Haupt <julian.haupt@hauptmedia.de>
2016-03-16 20:32:52 +0100

    correctly set headers for android download

--- src/MobileAppPackagingTools/AndroidResponseFactory.php
+++ src/MobileAppPackagingTools/AndroidResponseFactory.php
@@ -14,9 +14,12 @@ class AndroidResponseFactory {
     public static function createApkDownloadResponse($filename) {
         $response = new Response();
 
+        $response->headers->set('Content-Type', 'application/vnd.android.package-archive');
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Content-Disposition', 'attachment; filename="'.basename($filename).'"');
         $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
         $response->headers->set('Pragma', 'no-cache');
-        $response->headers->set('Content-Type', 'application/vnd.android.package-archive; charset=utf-8');
+        $response->headers->set('Content-Length', filesize($filename));
 
         $response->setContent(file_get_contents($filename));
 
